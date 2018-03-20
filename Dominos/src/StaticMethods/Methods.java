214db0de769df142da13d3c9e0b1826ec82f9366
Yanislav Mitev <qnislav_96_08@abv.bg>
2018-03-20 19:31:21 +0200

    IUser interface added with base functionalities

--- Dominos/src/StaticMethods/Methods.java
+++ Dominos/src/StaticMethods/Methods.java
@@ -1,6 +1,10 @@
 package StaticMethods;
 
 public abstract class Methods {
+	private static final int MAX_PASS_LENGTH = 25;
+	private static final int MIN_PASS_LENGTH = 5;
+
+
 	public static boolean checkString(String text) {
 		if(text != null && text.trim().length() > 0) {
 			return true;
@@ -12,4 +16,13 @@ public abstract class Methods {
 			return true;
 		}return false;
 	}
+
+
+	public static boolean checkPassword(String password) {
+		if(checkString(password) && password.length() > MIN_PASS_LENGTH && password.length() < MAX_PASS_LENGTH) {
+			return true;
+		}
+		return false;
+	}
+	
 }
---
+++ Dominos/src/bg/Dominos/Demo.java
@@ -0,0 +1,15 @@
+package bg.Dominos;
+
+import CustomExceptions.IllegalEMailException;
+import CustomExceptions.IllegalNameException;
+import CustomExceptions.IllegalPasswordException;
+import StaticMethods.Methods;
+
+public class Demo {
+
+	public static void main(String[] args) throws IllegalPasswordException, IllegalEMailException, IllegalNameException {
+		User ivan = new User("Ivan", "Ivanov", "ivan.ivanov@gmail.com", "ivanSexyPichagata69");
+		User mivan = new User("MIvan", "MIvanov", "mivan.mivanov@gmail.com", "mivanSexyPichagata69");
+	}
+
+}
